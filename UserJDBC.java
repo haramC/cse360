66923118d87c51eadbf1668c56d77410c1b6342f
haramC <90118253+haramC@users.noreply.github.com>
2021-11-11 19:38:21 -0700

    Test database

---
+++ UserJDBC.java
@@ -0,0 +1,26 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UserJDBC {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-user","root","7942zxcasD");
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from user");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("id"));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
